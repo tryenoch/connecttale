b201d1e392547bf61fae73fe0a9ef8b8556ecca5
jeti11 <jeti11@naver.com>
2023-08-02 17:10:14 +0900

    novel 관련 페이지 추가

--- src/main/frontend/src/novel/Novel.jsx
+++ src/main/frontend/src/novel/Novel.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
+import NovelDetail from "./NovelDetail";
 
 function novel(props) {
 
     return (
         <div>
-            <h1>novel</h1>
-            <Link to={'/login'}>login</Link><br/>
-            <Link to={'/join'}>join</Link><br/>
-            <Link to={'/myPage'}>mypage</Link><br/>
-            <Link to={'/board'}>board</Link><br/>
+          <h1>novel</h1>
+          <Link to={'/login'}>login</Link><br/>
+          <Link to={'/join'}>join</Link><br/>
+          <Link to={'/myPage'}>mypage</Link><br/>
+          <Link to={'/board'}>board</Link><br/>
+          <Link to={'/novelDetail'}>novel detail</Link>
+          
         </div>
     )
 }
--- src/main/java/com/bitc/full505_final_team4/controller/TestController.java
+++ src/main/java/com/bitc/full505_final_team4/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.bitc.full505_final_team4.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 public class TestController {
 
@@ -13,4 +16,5 @@ public class TestController {
     public List<String> hello() {
         return Arrays.asList("안녕하세요", "Hello");
     }
+
 }
