fb896e77b4032f4fecc80c42ca1e4ca1de163854
wook <sso1604@naver.com>
2023-08-03 17:11:48 +0900

    회원가입 페이지 중 성인인지 미성년자인지 선택하는 부분 UI 구현 완료 => 회원 정보 입력부분 UI 구현 예정

--- src/main/frontend/src/AppPage.jsx
+++ src/main/frontend/src/AppPage.jsx
@@ -7,7 +7,7 @@ import NovelDetail from "./novel/NovelDetail";
 import NovelSearch from "./novel/NovelSearch";
 import Board from "./board/Board";
 import MyPage from "./member/MyPage";
-import Join from "./member/Join";
+import Join1 from "./member/Join1";
 import Login from "./member/Login";
 import Footer from "./layout/Footer";
 import Main from "./Main";
@@ -26,7 +26,7 @@ function AppPage(props) {
           <Route path={'/novelSearch'} element={<NovelSearch/>}/>
           <Route path={'/board'} element={<Board/>}/>
           <Route path={'/myPage'} element={<MyPage/>}/>
-          <Route path={'/join'} element={<Join/>}/>
+          <Route path={'/join'} element={<Join1/>}/>
           <Route path={'/login'} element={<Login/>}/>
         </Routes>
       </Container>
--- src/main/frontend/src/member/Join1.jsx
+++ src/main/frontend/src/member/Join1.jsx
@@ -1,23 +1,53 @@
 import React from 'react';
 import '../static/css/style.css'
-import JoinLogoTop1 from "./JoinLogoTop1";
 import JoinLogoTop2 from "./JoinLogoTop2";
 import JoinLogoTop3 from "./JoinLogoTop3";
+import JoinLogoTop1 from "./JoinLogoTop1";
 
 function Join1(props) {
 
     return (
-        <div className={'container'}>
+        <div className={'container mt-5'}>
             <h1 className={'text-center'}>회원가입</h1>
             <div className={'row justify-content-center'}>
                 <div className={'col-sm-10'}>
-                    <JoinLogoTop1/>
-                    <JoinLogoTop2/>
-                    <JoinLogoTop3/>
+                    <div className={'row justify-content-center'}>
+                        <div className={'col-sm-10'}>
+                            <JoinLogoTop1/>
+                        </div>
+                    </div>
+                    <div className={'row mt-4 ms-4 mb-2'}>
+                        <div className={'col-sm-10 ms-auto'}>
+                            <h2>약관 동의</h2>
+                        </div>
+                    </div>
+                    <div className={'row justify-content-center mt-5'}>
+                        <button className={'btn btn-outline-primary block m-3'} type="button" id="upper14"
+                                name="14YO">14세 이상<br/>
+                            <i className="bi bi-person fs-button"></i>
+                        </button>
+                        <button className={'btn btn-outline-primary-low block m-3'} type="button" id="lower14"
+                                name="14YO">14세 미만 <br/>
+                            <i className="bi bi-people fs-button"></i>
+                        </button>
+                    </div>
+                    <div className={'row mt-3 justify-content-center mt-5'}>
+                        <div className={'col-sm-10'}>
+                            <div className={'row justify-content-center'}>
+                                <div className={'col-sm-8 ms-auto text-center'}>
+                                    <input type="checkbox" id={'useOk'}/>
+                                    <label htmlFor="useOk" className={'ms-2'}>
+                                        <span className={'text-color-red'}>[필수]</span>
+                                        개인정보 수집 및 이용동의
+                                    </label>
+                                </div>
+                                <div className={'col-sm-4'}>
+                                    <a href="#" className={'text-decoration-none ms-5'}>[전문보기]</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <input type="radio" />
             </div>
         </div>
     )
