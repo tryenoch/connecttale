65f5729d9a15d9f38e11aece38ce8c23f7623dc8
wook <sso1604@naver.com>
2023-08-03 12:46:24 +0900

    Join 페이지 로고 추가, 형태 변경

--- src/main/frontend/src/member/Join.jsx
+++ src/main/frontend/src/member/Join.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import joinLogo1 from "./joinLogo1non.svg";
-import "../static/css/style.css"
+import joinLogo1non from "./joinLogo1non.svg";
+import JoinLogo from "./JoinLogo1";
+import joinLogo1 from "./joinLogo1.svg";
 
 function Join(props) {
 
@@ -11,20 +12,27 @@ function Join(props) {
                 <div className={'col-sm-10'}>
                     <div className={'row my-4'}>
                         <div className={'col-sm-4 d-grid justify-content-center'}>
-                            <span className={'fs-6 text-secondary'}>STEP</span>
-                            <span className={'text-center fs-6 text-secondary'}>01</span>
-                            <img src={joinLogo1} alt="logo" className={'ImgSize text-secondary'} />
-                            <span className={'text-secondary'}>회원가입</span>
+                            <span className={'text-center fs-6'}>STEP</span>
+                            <span className={'text-center fs-6'}>01</span>
+                            <img src={joinLogo1} alt="logo" className={'ImgSize'}/>
+                            <span className={'text-center fs-7'}>회원가입</span>
                         </div>
-                        <div className={'col-sm-4 d-flex justify-content-center'}>
-
+                        <div className={'col-sm-4 d-grid justify-content-center'}>
+                            <span className={'text-center fs-6 text-secondary'}>STEP</span>
+                            <span className={'text-center fs-6 text-secondary'}>02</span>
+                            <img src={joinLogo1non} alt="logo" className={'ImgSize'} />
+                            <span className={'text-center text-secondary fs-7'}>정보입력</span>
                         </div>
-                        <div className={'col-sm-4 d-flex justify-content-center'}>
-
+                        <div className={'col-sm-4 d-grid justify-content-center'}>
+                            <span className={'text-center fs-6 text-secondary'}>STEP</span>
+                            <span className={'text-center fs-6 text-secondary'}>03</span>
+                            <img src={joinLogo1non} alt="logo" className={'ImgSize'} />
+                            <span className={'text-center text-secondary fs-7'}>가입완료</span>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
