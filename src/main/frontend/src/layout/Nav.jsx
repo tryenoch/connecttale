5e4b456eea9284d45acd3a9f96f6ab5b9d3154e1
chanmi <jhkk345@naver.com>
2023-08-03 14:43:16 +0900

    0803 / nav container class 추가

--- src/main/frontend/src/layout/Nav.jsx
+++ src/main/frontend/src/layout/Nav.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Navbar} from "react-bootstrap";
+import {Container, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 function Nav(props) {
   return (
     <Navbar expand={"lg"}>
-      <Link to={"/board"}>
-        <h3>네비게이션 링크 이동 테스트</h3>
-      </Link>
+      <Container>
+        <Link to={"/board"}>
+          <h3>네비게이션 링크 이동 테스트</h3>
+        </Link>
+      </Container>
     </Navbar>
   )
 }
