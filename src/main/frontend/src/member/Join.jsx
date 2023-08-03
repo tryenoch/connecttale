84dfc958473ba067e58119a08d564e8367bf5bd0
wook <sso1604@naver.com>
2023-08-03 12:11:56 +0900

    임시 커밋..

--- src/main/frontend/src/member/Join.jsx
+++ src/main/frontend/src/member/Join.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import joinLogo1 from "./joinLogo1.svg";
+import joinLogo1 from "./joinLogo1non.svg";
 import "../static/css/style.css"
 
 function Join(props) {
@@ -10,17 +10,16 @@ function Join(props) {
             <div className={'row justify-content-center'}>
                 <div className={'col-sm-10'}>
                     <div className={'row my-4'}>
-                        <div className={'col-sm-3 d-grid justify-content-center'}>
-                            <span>STEP</span>
-                            <img src={joinLogo1} alt="logo" className={'ImgSize'} />
+                        <div className={'col-sm-4 d-grid justify-content-center'}>
+                            <span className={'fs-6 text-secondary'}>STEP</span>
+                            <span className={'text-center fs-6 text-secondary'}>01</span>
+                            <img src={joinLogo1} alt="logo" className={'ImgSize text-secondary'} />
+                            <span className={'text-secondary'}>회원가입</span>
                         </div>
-                        <div className={'col-sm-3 d-flex justify-content-center'}>
+                        <div className={'col-sm-4 d-flex justify-content-center'}>
 
                         </div>
-                        <div className={'col-sm-3 d-flex justify-content-center'}>
-
-                        </div>
-                        <div className={'col-sm-3 d-flex justify-content-center'}>
+                        <div className={'col-sm-4 d-flex justify-content-center'}>
 
                         </div>
                     </div>
