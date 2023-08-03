3dc0bb3f834d3a7861b3251db9b4bfe82db292fe
jeti11 <jeti11@naver.com>
2023-08-03 16:53:06 +0900

    2023-08-03 학원, 플랫폼엔티티 클래스 생성

--- src/main/java/com/bitc/full505_final_team4/data/entity/NovelPlatformEntity.java
+++ src/main/java/com/bitc/full505_final_team4/data/entity/NovelPlatformEntity.java
@@ -1,4 +1,19 @@
 package com.bitc.full505_final_team4.data.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "platform")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class NovelPlatformEntity {
+  @Id
+  private int platform;
+
 }
