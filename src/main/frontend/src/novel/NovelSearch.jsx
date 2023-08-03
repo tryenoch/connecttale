5b47a8ed9d8368d2fd850be4c072ece08921d057
jeti11 <jeti11@naver.com>
2023-08-03 12:46:43 +0900

    서치 페이지 추가

--- src/main/frontend/src/novel/NovelSearch.jsx
+++ src/main/frontend/src/novel/NovelSearch.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function NovelSearch(props) {
+  const [searchWord, setSearchWord] = useState();
+  
+  useEffect(() => {
+    // 검색란에 입력한 단어로 searchWord를 수정해야함
+    setSearchWord();
+  }, [])
+  
+  
   
   return (
-    <div></div>
+    <div className={'container my-4'}>
+      <h5 className={'fw-bold'}>'{searchWord}' 검색 결과</h5>
+      
+    </div>
   )
 }
 
