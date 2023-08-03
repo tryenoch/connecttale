dc02727c01c7300a6da3a30529d365eeffc42c52
sein <jhkk345@naver.com>
2023-08-03 09:42:26 +0900

    0803 / jpa 초기 설정, 폴더 추가

---
+++ src/main/java/com/bitc/full505_final_team4/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.bitc.full505_final_team4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
