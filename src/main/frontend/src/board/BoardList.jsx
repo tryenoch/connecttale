fa5d41047d8e83c9cbac1868295b0b8973b0390c
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-04 12:39:11 +0900

    board 페이지 table 구현

--- src/main/frontend/src/board/BoardList.jsx
+++ src/main/frontend/src/board/BoardList.jsx
@@ -3,8 +3,9 @@ import {Col, Form, Row} from "react-bootstrap";
 
 function BoardList(props) {
 
-    const [keyword, setKeyword] = useState('');
+    //페이지가 변할 때 cate 값 0으로 초기화
     const [cate, setCate] = useState(0);
+    const [keyword, setKeyword] = useState('');
 
     const HandleSubmit = (event) => {
         alert(`검색어 : ${keyword}, 종류 : ${cate}`);
@@ -22,13 +23,12 @@ function BoardList(props) {
                             <Row>
                                 <Col className={'px-0'}>
                                     <Form.Select
-                                        className={'border-2 border-black select-box fw-bold'}
+                                        className={'border-2 border-black select-box cursor fw-bold'}
                                         value={cate}
                                         onChange={(e) => setCate(e.target.value)}
                                     >
                                         <option value={0}>제목</option>
                                         <option value={1}>작성자</option>
-                                        <option value={2}>등록일</option>
                                     </Form.Select>
                                 </Col>
                                 <Col xs={8} className={'search-bar'}>
@@ -43,6 +43,52 @@ function BoardList(props) {
                             </Row>
                         </form>
                     </Col>
+                </Row>
+                <Row>
+                    <table className={'text-center table'}>
+                        <colgroup>
+                            <col width={'10%'}/>
+                            <col width={'60%'}/>
+                            <col width={'15%'}/>
+                            <col width={'15%'}/>
+                        </colgroup>
+                        <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            <th>날짜</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td className={'text-start cursor'}>비밀글 입니다.</td>
+                            <td>tester1</td>
+                            <td>2023.08.04</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td className={'text-start cursor'}>비밀글 입니다.</td>
+                            <td>tester1</td>
+                            <td>2023.08.04</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td className={'text-start cursor'}>비밀글 입니다.</td>
+                            <td>tester1</td>
+                            <td>2023.08.04</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div className={'d-flex justify-content-center mx-auto my-3'}>
+                        <button>처음 페이지</button>
+                        <button>이전 페이지</button>
+                        <button>1</button>
+                        <button>다음 페이지</button>
+                        <button>마지막 페이지</button>
+
+                    </div>
                 </Row>
             </Col>
         </Row>
