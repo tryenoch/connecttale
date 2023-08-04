baadf1fd8e151908051f9c8440f21235a7935f7c
joontae98_mac <jmt2066206@gmail.com>
2023-08-04 09:42:36 +0900

    Tabs 마우스 커서 모양 수정

--- src/main/frontend/src/board/Board.jsx
+++ src/main/frontend/src/board/Board.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Col, Container, Nav, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 const boardList = [
     {
@@ -39,7 +39,7 @@ function Board(props) {
                                     key={value.id}
                                     className={'nav-item'}>
                                     <a
-                                        className={index === selectBoard ? 'nav-link active selected' : 'nav-link unselected'}
+                                        className={index === selectBoard ? 'nav-link active selected cursor' : 'nav-link unselected cursor'}
                                         onClick={() => selectTabHandler(index)}>{value.title}</a>
                                 </li>
                             })
