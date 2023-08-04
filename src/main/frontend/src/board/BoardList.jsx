4a5ce7b97f1e79598c7bd60fe150584edf702d06
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-04 11:41:56 +0900

    드롭박스 구현

--- src/main/frontend/src/board/BoardList.jsx
+++ src/main/frontend/src/board/BoardList.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import {Col, Row} from "react-bootstrap";
+import React, {useState} from 'react';
+import {Col, Form, Row} from "react-bootstrap";
 
 function BoardList(props) {
+
+    const [keyword, setKeyword] = useState('');
+    const [cate, setCate] = useState(0);
+
+    const HandleSubmit = (event) => {
+        alert(`검색어 : ${keyword}, 종류 : ${cate}`);
+        event.preventDefault();
+
+    }
 
     return (
         <Row>
@@ -9,16 +18,29 @@ function BoardList(props) {
                 <Row className={'border-3 border-black border-bottom py-2'}>
                     <Col className={'ps-0'}><h3 className={'fw-bold'}>{props.data.title}</h3></Col>
                     <Col className={'pe-0'}>
-                        <Row>
-                            <form>
+                        <form onSubmit={HandleSubmit}>
+                            <Row>
                                 <Col>
+                                    <Form.Select
+                                        value={cate}
+                                        onChange={(e) => setCate(e.target.value)}
+                                    >
+                                        <option value={0}>제목</option>
+                                        <option value={1}>작성자</option>
+                                        <option value={2}>등록일</option>
+                                    </Form.Select>
                                 </Col>
                                 <Col xs={8} className={'search-bar'}>
-                                    <input type={'text'} placeholder={'검색어를 입력하세요'}/>
+                                    <input
+                                        type={'text'}
+                                        value={keyword}
+                                        placeholder={'검색어를 입력하세요'}
+                                        onChange={(e) => setKeyword(e.target.value)}
+                                    />
                                     <button type={'submit'}><i className="bi bi-search"></i></button>
                                 </Col>
-                            </form>
-                        </Row>
+                            </Row>
+                        </form>
                     </Col>
                 </Row>
             </Col>
