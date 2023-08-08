f8e2ac6ed887b3deb838dc20765bca422f2c0889
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-08 11:22:13 +0900

    board, board_cate, req_cate Entity 파일 추가

---
+++ src/main/java/com/bitc/full505_final_team4/data/entity/BoardEntity.java
@@ -0,0 +1,45 @@
+package com.bitc.full505_final_team4.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int boardIdx;
+
+    @Column(nullable = false, length = 100)
+    private String boardTitle;
+
+    @Column(nullable = false, length = 200)
+    private String boardContents;
+
+    @ManyToOne
+    @JoinColumn(name = "board_cate", nullable = false)
+    @ToString.Exclude
+    private BoardCateEntity boardCate;
+
+    @Column(nullable = false)
+    private String createId;
+
+    @Column(nullable = false)
+    private LocalDateTime createDt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "req_cate", nullable = true)
+    @ToString.Exclude
+    private ReqCateEntity reqCate;
+
+}
---
+++ src/main/java/com/bitc/full505_final_team4/data/entity/ReqCateEntity.java
@@ -0,0 +1,31 @@
+package com.bitc.full505_final_team4.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "req_cate")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ReqCateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+
+    @Column(nullable = false, length = 100)
+    private String reqName;
+
+    @OneToMany(mappedBy = "reqCate")
+    @ToString.Exclude
+    private List<BoardEntity> boardList = new ArrayList<>();
+
+}
