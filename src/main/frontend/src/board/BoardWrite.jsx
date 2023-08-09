c91929708ce38ed30403c1fecde0b4224e034c9a
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-09 12:51:29 +0900

    ok스마트 에디터 라이브러리 추가

--- src/main/frontend/src/board/BoardWrite.jsx
+++ src/main/frontend/src/board/BoardWrite.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
 
 function BoardWrite() {
 
     return (
-        <div>
-            <h1>boardWrite</h1>
-        </div>
+        <Container className={'my-4'}>
+            <h2>boardWrite</h2>
+            <Row>
+                <Col xs={10} className={'my-5 mx-auto'}>
+                    <Row className={'border-3 border-black border-bottom py-2'}>
+                        <Col className={'ps-0'}>
+                            <h3 className={'fw-bold'}>글 쓰기</h3>
+                        </Col>
+                        <form>
+
+                        </form>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
