75372832e28447fc96b87243e899e470d0c91c28
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-09 13:46:19 +0900

    boardWirte 페이지에 okEditor 구현

--- src/main/frontend/src/board/BoardWrite.jsx
+++ src/main/frontend/src/board/BoardWrite.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import {CKEditor} from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function BoardWrite() {
 
@@ -8,14 +10,33 @@ function BoardWrite() {
             <h2>boardWrite</h2>
             <Row>
                 <Col xs={10} className={'my-5 mx-auto'}>
-                    <Row className={'border-3 border-black border-bottom py-2'}>
+                    <Row className={'border-3 border-black border-bottom py-2 mb-3'}>
                         <Col className={'ps-0'}>
                             <h3 className={'fw-bold'}>글 쓰기</h3>
                         </Col>
-                        <form>
-
-                        </form>
                     </Row>
+                    <form>
+                        <CKEditor
+                            editor={ClassicEditor}
+                            config={{
+                                placeholder: "내용을 입력하세요.",
+                            }}
+                            onReady={editor => {
+                                // You can store the "editor" and use when it is needed.
+                                console.log('Editor is ready to use!', editor);
+                            }}
+                            onChange={(event, editor) => {
+                                const data = editor.getData();
+                                console.log({event, editor, data});
+                            }}
+                            onBlur={(event, editor) => {
+                                console.log('Blur.', editor);
+                            }}
+                            onFocus={(event, editor) => {
+                                console.log('Focus.', editor);
+                            }}
+                        />
+                    </form>
                 </Col>
             </Row>
         </Container>
