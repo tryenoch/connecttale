71af78f245fc633e82cb03d0b466992cd7047d24
joontae98_win <jmt2066206@gmail.com>
2023-08-13 22:00:35 +0900

    BoardReplyDTO 추가

---
+++ src/main/java/com/bitc/full505_final_team4/data/dto/BoardReplyDTO.java
@@ -0,0 +1,10 @@
+package com.bitc.full505_final_team4.data.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardReplyDTO {
+    private int boardIdx;
+    private String createId;
+    private String reply;
+}
