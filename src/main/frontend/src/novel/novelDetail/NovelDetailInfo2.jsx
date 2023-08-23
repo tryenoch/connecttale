12d5ac182de6570610dbfd58225028731ec1ddb2
enoch012 <zzan6238@naver.com>
2023-08-24 00:37:54 +0900

    0823 / detail ui 추가수정

--- src/main/frontend/src/novel/novelDetail/NovelDetailInfo2.jsx
+++ src/main/frontend/src/novel/novelDetail/NovelDetailInfo2.jsx
@@ -4,13 +4,14 @@ import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import {hr} from "date-fns/locale";
 import {CategoryConverter} from "../../common/NovelInfoConverter";
+import {Col, Row} from "react-bootstrap";
 
 
 function NovelDetailInfo(props) {
   const [title, setTitle] = useState(encodeURIComponent(props.novelDetail.novelIdx.novelTitle));
   const [ebookCheck, setEbookCheck] = useState(props.novelDetail.novelIdx.ebookCheck);
   const [novelAdult, setNovelAdult] = useState(props.novelDetail.novelIdx.novelAdult);
-  
+
   // 랜더링에 사용되는 state 들
   const [novelInfo, setNovelInfo] = useState(props.novelDetail);
   const [ridi, setRidi] = useState({});
@@ -24,12 +25,12 @@ function NovelDetailInfo(props) {
   console.log(baseItem.cateList);
   let cateList = "";
 
-  if (baseItem.cateList == null || baseItem.cateList == undefined){
+  if (baseItem.cateList == null || baseItem.cateList == undefined) {
     cateList = "";
   } else {
     cateList = CategoryConverter(baseItem.cateList);
   }
-  
+
   useEffect(() => {
     axios.get('/novelDetail', {
       params:
@@ -42,7 +43,7 @@ function NovelDetailInfo(props) {
       .then(res => {
         setNovelInfo(res.data)
         console.log(novelInfo);
-        
+
         // 소설 디테일 정보 setState 해주는 곳
         if (novelInfo.ridi) {
           setBaseItem(novelInfo.ridi);
@@ -51,7 +52,7 @@ function NovelDetailInfo(props) {
         } else if (novelInfo.kakao) {
           setBaseItem(novelInfo.kakao);
         }
-        
+
         if (novelInfo.ridi != undefined && novelInfo.naver != undefined && novelInfo.kakao != undefined) {
           setRidi(novelInfo.ridi);
           setNaver(novelInfo.naver);
@@ -72,25 +73,25 @@ function NovelDetailInfo(props) {
         } else if (novelInfo.kakao != undefined) {
           setKakao(novelInfo.kakao);
         }
-        
+
 
       })
       .catch(err => {
         console.log(err.message);
       })
-    
+
   }, []);
 
   let heartSelect;
   const heart = novelInfo.novelLikeList.map((item) => (
-      item.id.id == loginId && item.likeYn == 'Y' ? '찜' : null
+    item.id.id == loginId && item.likeYn == 'Y' ? '찜' : null
   ));
 
-  if(heart.indexOf('찜') > -1) {
+  if (heart.indexOf('찜') > -1) {
     heartSelect = true;
   }
-  
-  
+
+
   // 좋아요 버튼 클릭이벤트
   const likeClickHandler = () => {
     // 세션 값이 존재하면 좋아요 버튼 동작
@@ -103,10 +104,10 @@ function NovelDetailInfo(props) {
         }
       })
         .then(async (res) => {
-          
+
           // console.log(res);
           // db의 작품 정보(제목, 저자, 별점 등)와 함께 좋아요 관련 정보를 받아오기 위한 axios통신
-          
+
           const res2 = await axios.get('/novelDetail', {
             params: {
               title: title,
@@ -114,7 +115,7 @@ function NovelDetailInfo(props) {
               ageGrade: novelAdult
             }
           })
-          
+
           setNovelInfo(res2.data);
           console.log(novelInfo);
 
@@ -128,11 +129,11 @@ function NovelDetailInfo(props) {
       alert("회원만 이용 가능한 서비스입니다.");
     }
   }
-  
+
   return (
     <div>
       <div className={'row'}>
-        <div className={'col-sm-4'}>
+        <div className={'col-sm-4 align-items-start img-area'}>
           <div className={'detail-image'}>
             <img src={baseItem.novelThumbnail} alt="" className={'w-100'}/>
           </div>
@@ -183,48 +184,49 @@ function NovelDetailInfo(props) {
               <div className={'d-flex align-items-center me-3'}>
                 {
                   Object.keys(kakao).length != 0
-                      ? <span>
+                    ? <span>
                     <img src="/kakao-page-icon.png" alt="" className={'platform-logo'}/>
                     <span className={'mx-2 novel-score'}>{kakao.novelStarRate}</span>
                     <span className={'rating-base'}>/ 10점</span>
                   </span>
-                      : null
+                    : null
                 }
               </div>
               <div className={'d-flex align-items-center me-3'}>
                 {
                   Object.keys(naver).length != 0
-                      ? <span>
+                    ? <span>
                       <img src="/naver-series-icon.png" alt="" className={'platform-logo'}/>
                       <span className={'mx-2 novel-score'}>{naver.novelStarRate}</span>
                       <span className={'rating-base'}>/ 10점</span>
                     </span>
-                      : null
+                    : null
                 }
               </div>
               <div className={'d-flex align-items-center me-3'}>
                 {
                   Object.keys(ridi).length != 0
-                      ? <span>
+                    ? <span>
                       <img src="/ridibooks-icon.jpg" alt="" className={'platform-logo'}/>
                       <span className={'mx-2 novel-score'}>{ridi.novelStarRate}</span>
                       <span className={'rating-base'}>/ 5점</span>
                     </span>
-                      : null
+                    : null
                 }
               </div>
             </div>
 
 
-            
             {/*출판사, 저자, 총화수, 완결유무, 연재요일, 출판일*/}
             <div className={'my-1'}>
               <p className={'fw-bold'}>{baseItem.novelAuthor}<span className={'ms-2 text-muted'}>저</span></p>
               <p className={'fw-bold'}>{baseItem.novelPubli}<span className={'ms-2 text-muted'}>출판</span></p>
               {
                 baseItem.ebookCheck == '웹소설'
-                  ? <p className={'fw-bold'}>총 {baseItem.novelCount} 화 / {baseItem.novelCompleteYn == 'Y' ? '완결' : baseItem.novelUpdateDate ? `연재중 | ${baseItem.novelUpdateDate}` : '연재중'}</p>
-                  : <p className={'fw-bold'}>총 {baseItem.novelCount} 권 / {baseItem.novelCompleteYn == 'Y' ? '완결' : baseItem.novelUpdateDate ? `연재중 | ${baseItem.novelUpdateDate}` : '연재중'}</p>
+                  ? <p className={'fw-bold'}>총 {baseItem.novelCount} 화
+                    / {baseItem.novelCompleteYn == 'Y' ? '완결' : baseItem.novelUpdateDate ? `연재중 | ${baseItem.novelUpdateDate}` : '연재중'}</p>
+                  : <p className={'fw-bold'}>총 {baseItem.novelCount} 권
+                    / {baseItem.novelCompleteYn == 'Y' ? '완결' : baseItem.novelUpdateDate ? `연재중 | ${baseItem.novelUpdateDate}` : '연재중'}</p>
               }
               {
                 baseItem.novelRelease
@@ -238,41 +240,41 @@ function NovelDetailInfo(props) {
                   <span className={'ms-2'}> ※ 클릭하시면 해당 플랫폼으로 이동합니다.</span>
                 </div>
                 <table className={'table table-hover detail-price-table'}>
-                      {
-                        Object.keys(kakao).length != 0
-                            ?
-                            <tr>
-                              <Link to={`https://page.kakao.com/content/${kakao.platformId}`}>
-                              <td>
+                  {
+                    Object.keys(kakao).length != 0
+                      ?
+                      <tr>
+                        <Link to={`https://page.kakao.com/content/${kakao.platformId}`} target={'_blank'}>
+                          <td>
                             <p><img src="/kakao-page-icon.png" alt="" className={'platform-logo me-2'}/>카카오페이지 <span
-                                className={'novel-price'}>{kakao.novelPrice}</span>원</p>
-                              </td>
-                              </Link>
-                              </tr>
-                            : null
-                      }
-                      {
-                        Object.keys(naver).length != 0
-                            ? <tr>
-                              <Link to={`https://series.naver.com/novel/detail.series?productNo=${naver.platformId}`}>
-                              <td><p><img src="/naver-series-icon.png" alt="" className={'platform-logo me-2'}/>네이버시리즈 <span
-                                className={'novel-price'}>{naver.novelPrice}</span>쿠키</p>
-                                </td>
-                              </Link>
-                            </tr>
-                            : null
-                      }
-                      {
-                        Object.keys(ridi).length != 0
-                            ? <tr>
-                              <Link to={`https://ridibooks.com/books/${ridi.platformId}`}>
-                              <td><p><img src="/ridibooks-icon.jpg" alt="" className={'platform-logo me-2'}/>리디북스 <span
-                                className={'novel-price'}>{ridi.novelPrice}</span>원</p>
-                            </td>
-                              </Link>
-                            </tr>
-                            : null
-                      }
+                              className={'novel-price'}>{kakao.novelPrice}</span>원</p>
+                          </td>
+                        </Link>
+                      </tr>
+                      : null
+                  }
+                  {
+                    Object.keys(naver).length != 0
+                      ? <tr>
+                        <Link to={`https://series.naver.com/novel/detail.series?productNo=${naver.platformId}`}>
+                          <td><p><img src="/naver-series-icon.png" alt="" className={'platform-logo me-2'} target={'_blank'}/>네이버시리즈 <span
+                            className={'novel-price'}>{naver.novelPrice}</span>쿠키</p>
+                          </td>
+                        </Link>
+                      </tr>
+                      : null
+                  }
+                  {
+                    Object.keys(ridi).length != 0
+                      ? <tr>
+                        <Link to={`https://ridibooks.com/books/${ridi.platformId}`} target={'_blank'}>
+                          <td><p><img src="/ridibooks-icon.jpg" alt="" className={'platform-logo me-2'}/>리디북스 <span
+                            className={'novel-price'}>{ridi.novelPrice}</span>원</p>
+                          </td>
+                        </Link>
+                      </tr>
+                      : null
+                  }
                 </table>
 
                 {/*<div className={'border p-3 rounded-3 my-2'}>
@@ -315,20 +317,23 @@ function NovelDetailInfo(props) {
                       : null
                 }*/}
               </div>
-              </div>
-
+            </div>
 
 
           </div>
         </div>
       </div>
       {/*작품 소개 글*/}
+      <Row>
+        <Col>
       <div className={'my-4 detail-intro'}>
         <h4 className={'detail-subtit'}>작품 소개</h4>
         <p className={'novel-desc px-0 pb-5'}>{baseItem.novelIntro}</p>
       </div>
+        </Col>
+      </Row>
     </div>
-  
+
   )
 }
 
