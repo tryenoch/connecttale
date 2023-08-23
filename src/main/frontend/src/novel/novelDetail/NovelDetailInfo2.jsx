47c03087a40cd4c73bd29a46680e6d5d1b7b9e1f
jeti11 <jeti11@naver.com>
2023-08-23 16:21:33 +0900

    디테일페이지 본인 좋아요 작품 '찜'표시

--- src/main/frontend/src/novel/novelDetail/NovelDetailInfo2.jsx
+++ src/main/frontend/src/novel/novelDetail/NovelDetailInfo2.jsx
@@ -20,16 +20,6 @@ function NovelDetailInfo(props) {
   // 세션id값 받아오기, 세션 정보 없으면 noUser가 됨 -> 서버에서 memberEntity 못찾게끔
   const [loginId, setLoginId] = useState(sessionStorage.getItem("id") ? sessionStorage.getItem("id") : 'noUser');
   
-  // const [likeFlag, setLikeFlag] = useState(false);
-  // const [novelLikeList, setNovelLikeList] = useState([]);
-  // const [novelLikeCount, setNovelLikeCount] = useState(0);
-  
-
-  
-  // console.log(kakao);
-  // console.log(naver);
-  // console.log(ridi);
-  // console.log(baseItem);
   
   useEffect(() => {
     axios.get('/novelDetail', {
@@ -74,16 +64,6 @@ function NovelDetailInfo(props) {
           setKakao(novelInfo.kakao);
         }
         
-        // for (let i = 0; i < novelInfo.novelLikeList.length; i++) {
-        //   if (novelInfo.novelLikeList[i].likeYn == 'Y' && novelInfo.novelLikeList[i].id.id == loginId) {
-        //     setLikeFlag(true);
-        //     break;
-        //   }
-        //   else {
-        //     setLikeFlag(false);
-        //   }
-        // }
-        
 
       })
       .catch(err => {
@@ -119,29 +99,17 @@ function NovelDetailInfo(props) {
           
           setNovelInfo(res2.data);
           console.log(novelInfo);
-  
-          // for (let i = 0; i < novelInfo.novelLikeList.length; i++) {
-          //   if (novelInfo.novelLikeList[i].likeYn == 'Y' && novelInfo.novelLikeList[i].id.id == loginId) {
-          //     setLikeFlag(true);
-          //     break;
-          //   }
-          //   else {
-          //     setLikeFlag(false);
-          //   }
-          // }
-          
+
         })
         .catch(err => {
           console.log(err.message);
         })
     }
-    // 세션 값이 없으면 로그인 화면으로 이동
+    // 세션 값이 없기 때문에 로그인하라는 알림창
     else {
       alert("회원만 이용 가능한 서비스입니다.");
     }
-    
   }
-  
   
   return (
     <div>
@@ -177,18 +145,6 @@ function NovelDetailInfo(props) {
                   {baseItem.novelTitle} [{baseItem.ebookCheck}] </p>
               </div>
               <div className={'col-sm-3'}>
-                {
-                  novelInfo.novelLikeList.map((item, index) => {
-                    return (
-                      <div key={index}>
-                        <p>좋아요 아이디 : {item.id.id}</p>
-                        <p>로그인 아이디 : {loginId}</p>
-                        <p>{item.likeYn}</p>
-                        <hr/>
-                      </div>
-                    )
-                  })
-                }
                 <button type={'button'} className={`btn-outline-purple ms-2`} onClick={likeClickHandler}>
                   <i className="bi bi-heart detail-heart"></i>
                   <span>
@@ -200,14 +156,7 @@ function NovelDetailInfo(props) {
                     }
                   </span>
                 </button>
-
                 
-                {/*<button type={'button'} className={likeFlag ? 'btn btn-outline-purple ms-2' : 'btn btn-purple ms-2'} onClick={likeClickHandler}>*/}
-                {/*  <i className="bi bi-heart detail-heart"></i>*/}
-                {/*  <span>*/}
-                {/*  {novelInfo.novelLikeCount}*/}
-                {/*  </span>*/}
-                {/*</button>*/}
               </div>
             </div>
             
