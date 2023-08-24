00333655d26507d6f3190525865705ee1152d1d8
enoch012 <zzan6238@naver.com>
2023-08-24 12:00:13 +0900

    0822 / 로그인, 회원가입 ui 수정

--- src/main/frontend/src/member/join/Join3.jsx
+++ src/main/frontend/src/member/join/Join3.jsx
@@ -7,21 +7,21 @@ function Join3(props) {
     return (
         <div className={'col-sm-10'}>
             <div className={'row justify-content-center'}>
-                <div className={'col-sm-10'}>
+                <div className={'col-sm-10 ps-0'}>
                     <JoinLogoTop3/>
                 </div>
             </div>
-            <div className={'my-5'}> <br/><br/></div>
-            <div className={'row mt-4 mb-5 justify-content-center'}>
-                <div className={'col-sm-10 text-center'}>
-                    <h2>축하드립니다</h2>
-                    <h6>회원가입이 완료되었습니다.</h6>
+            <div className={'my-5 pb-2'}></div>
+            <div className={'row mt-5 mb-5 justify-content-center'}>
+                <div className={'col-sm-10 text-center ps-0 join-sub-tit'}>
+                    <h1>축하드립니다</h1>
+                    <h5>회원가입이 완료되었습니다.</h5>
                 </div>
             </div>
-            <div className={'row justify-content-center ms-5'}>
-                <div className={'col-sm-3'}>
+            <div className={'row justify-content-center'}>
+                <div className={'col-sm-3 ps-0 text-center'}>
                     <Link to={'/'}>
-                        <button type={'button'} className={'btn btn-purple-inline'}>메인페이지로 돌아가기</button>
+                        <button type={'button'} className={'btn btn-outline-purple purple-round'}>메인페이지로 돌아가기</button>
                     </Link>
                 </div>
             </div>
