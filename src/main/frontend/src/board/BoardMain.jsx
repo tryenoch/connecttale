8e637f2e4fbcf1eccd187e68a2f1ef323daaf3c4
DESKTOP-96DHBRN\it <jmt2066206@gmail.com>
2023-08-24 10:13:37 +0900

    게시판 메인 마진 수정

--- src/main/frontend/src/board/BoardMain.jsx
+++ src/main/frontend/src/board/BoardMain.jsx
@@ -36,7 +36,7 @@ function BoardMain() {
   return (
       <Container className={'my-4'}>
         <Row>
-          <Col xs={6} className={'mx-auto mt-5'}>
+          <Col xs={6} className={'mx-auto'}>
             <ul className="nav nav-pills nav-fill gap-2 p-1 small rounded-5 shadow-sm">
               {
                 boardList.map((value, index) => {
